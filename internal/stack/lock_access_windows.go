523ee287125101491fb0b9b630b9eb960f31d76a
Andrew Lee <andyrewlee@gmail.com>
2026-07-06 02:09:13 -0700

    fix: classify windows access denied lock races

--- internal/stack/lock_access_windows.go
+++ internal/stack/lock_access_windows.go
@@ -8,8 +8,13 @@ import (
 	"syscall"
 )
 
-const windowsSharingViolation = syscall.Errno(32) // ERROR_SHARING_VIOLATION
+const (
+	windowsAccessDenied     = syscall.Errno(5)  // ERROR_ACCESS_DENIED
+	windowsSharingViolation = syscall.Errno(32) // ERROR_SHARING_VIOLATION
+)
 
 func retryableLockFileAccess(err error) bool {
-	return errors.Is(err, os.ErrPermission) || errors.Is(err, windowsSharingViolation)
+	return errors.Is(err, os.ErrPermission) ||
+		errors.Is(err, windowsAccessDenied) ||
+		errors.Is(err, windowsSharingViolation)
 }
